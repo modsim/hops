9b812f45537cceb21413efbf80054d4ee027d57e
Fredrik Jadebeck <fredrik@jadebeck.dev>
2020-05-26 00:27:46 +0200

    updated examples

--- examples/SamplingUniformTargetDemo.cpp
+++ examples/SamplingUniformTargetDemo.cpp
@@ -2,71 +2,54 @@
 #include <hops/FileWriter/FileWriterFactory.hpp>
 #include <hops/RandomNumberGenerator/RandomNumberGenerator.hpp>
 #include <hops/MarkovChain/MarkovChainAdapter.hpp>
-#include <hops/MarkovChain/Recorder/StateRecorder.hpp>
-#include <hops/MarkovChain/StateTransformation.hpp>
-#include <hops/MarkovChain/Proposal/CoordinateHitAndRunProposal.hpp>
-#include <hops/MarkovChain/Proposal/HitAndRunProposal.hpp>
-#include <hops/MarkovChain/Proposal/DikinProposal.hpp>
-#include <hops/Transformation/Transformation.hpp>
-#include <hops/MarkovChain/Draw/NoOpDrawAdapter.hpp>
-#include <hops/MarkovChain/Draw/MetropolisHastingsFilter.hpp>
+#include <hops/MarkovChain/MarkovChainFactory.hpp>
 
 int main() {
-    auto A = hops::CsvReader::readMatrix<Eigen::MatrixXd>("../resources/simplex_64D/A_simplex_64D_rounded.csv");
-    auto N = hops::CsvReader::readMatrix<Eigen::MatrixXd>("../resources/simplex_64D/N_simplex_64D_rounded.csv");
-    auto b = hops::CsvReader::readVector<Eigen::VectorXd>("../resources/simplex_64D/b_simplex_64D_rounded.csv");
-    auto s = hops::CsvReader::readVector<Eigen::VectorXd>("../resources/simplex_64D/start_simplex_64D_rounded.csv");
+    auto Arounded = hops::CsvReader::readMatrix<Eigen::MatrixXd>("../resources/simplex_64D/A_simplex_64D_rounded.csv");
+    auto Nrounded = hops::CsvReader::readMatrix<Eigen::MatrixXd>("../resources/simplex_64D/N_simplex_64D_rounded.csv");
+    auto brounded = hops::CsvReader::readVector<Eigen::VectorXd>("../resources/simplex_64D/b_simplex_64D_rounded.csv");
+    auto srounded = hops::CsvReader::readVector<Eigen::VectorXd>(
+            "../resources/simplex_64D/start_simplex_64D_rounded.csv");
     auto shift = hops::CsvReader::readVector<Eigen::VectorXd>(
             "../resources/simplex_64D/p_shift_simplex_64D_rounded.csv");
-    auto A2 = hops::CsvReader::readMatrix<Eigen::MatrixXd>("../resources/simplex_64D/A_simplex_64D_unrounded.csv");
-    auto b2 = hops::CsvReader::readVector<Eigen::VectorXd>("../resources/simplex_64D/b_simplex_64D_unrounded.csv");
-    auto s2 = hops::CsvReader::readVector<Eigen::VectorXd>("../resources/simplex_64D/start_simplex_64D_unrounded.csv");
+    auto Aunrounded = hops::CsvReader::readMatrix<Eigen::MatrixXd>(
+            "../resources/simplex_64D/A_simplex_64D_unrounded.csv");
+    auto bunrounded = hops::CsvReader::readVector<Eigen::VectorXd>(
+            "../resources/simplex_64D/b_simplex_64D_unrounded.csv");
+    auto sunrounded = hops::CsvReader::readVector<Eigen::VectorXd>(
+            "../resources/simplex_64D/start_simplex_64D_unrounded.csv");
 
-    auto markovChain1 = hops::MarkovChainAdapter(
-            hops::NoOpDrawAdapter(
-                    hops::StateRecorder(
-                            hops::StateTransformation(
-                                    hops::CoordinateHitAndRunProposal(
-                                            A,
-                                            b,
-                                            s),
-                                    hops::Transformation(N, shift))
-                    )
-            )
-    );
+    auto coordinateHitAndRun = hops::MarkovChainFactory::createMarkovChain(
+            hops::MarkovChainType::CoordinateHitAndRun,
+            Arounded,
+            brounded,
+            srounded,
+            Nrounded,
+            shift);
 
-    auto markovChain2 = hops::MarkovChainAdapter(
-            hops::NoOpDrawAdapter(
-                    hops::StateRecorder(
-                            hops::StateTransformation(
-                                    hops::HitAndRunProposal(
-                                            A,
-                                            b,
-                                            s),
-                                    hops::Transformation(N, shift))
-                    )
-            )
-    );
+    auto hitAndRun = hops::MarkovChainFactory::createMarkovChain(
+            hops::MarkovChainType::HitAndRun,
+            Arounded,
+            brounded,
+            srounded,
+            Nrounded,
+            shift);
 
-    auto markovChain3 = hops::MarkovChainAdapter(
-            hops::StateRecorder(
-                    hops::MetropolisHastingsFilter(
-                            hops::DikinProposal(
-                                    A2,
-                                    b2,
-                                    s2)
-                    )
-            )
-    );
+    auto dikinWalk = hops::MarkovChainFactory::createMarkovChain(
+            hops::MarkovChainType::DikinWalk,
+            Aunrounded,
+            bunrounded,
+            sunrounded);
 
 
     hops::RandomNumberGenerator randomNumberGenerator(42);
 
-    markovChain1.draw(randomNumberGenerator, 10000, 64);
-    markovChain2.draw(randomNumberGenerator, 10000, 64);
-    markovChain3.draw(randomNumberGenerator, 10000, 64);
+    coordinateHitAndRun->draw(randomNumberGenerator, 10000, 64);
+    hitAndRun->draw(randomNumberGenerator, 10000, 64);
+    dikinWalk->draw(randomNumberGenerator, 10000, 64);
 
-    markovChain1.writeHistory(hops::FileWriterFactory::createFileWriter("chrr", hops::FileWriterType::Csv).get());
-    markovChain2.writeHistory(hops::FileWriterFactory::createFileWriter("hrr", hops::FileWriterType::Csv).get());
-    markovChain3.writeHistory(hops::FileWriterFactory::createFileWriter("dikin", hops::FileWriterType::Csv).get());
+    coordinateHitAndRun->writeHistory(
+            hops::FileWriterFactory::createFileWriter("chrr", hops::FileWriterType::Csv).get());
+    hitAndRun->writeHistory(hops::FileWriterFactory::createFileWriter("hrr", hops::FileWriterType::Csv).get());
+    dikinWalk->writeHistory(hops::FileWriterFactory::createFileWriter("dikin", hops::FileWriterType::Csv).get());
 }
