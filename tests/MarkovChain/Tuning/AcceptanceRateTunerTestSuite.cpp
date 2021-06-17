a3efb1aed5237fc5d3461628f80aa66f54a17fc8
Fredrik Jadebeck <johann.fredrik@jadebeck.dev>
2021-06-18 01:06:45 +0200

    update acceptance rate tuner and add test

--- tests/MarkovChain/Tuning/AcceptanceRateTunerTestSuite.cpp
+++ tests/MarkovChain/Tuning/AcceptanceRateTunerTestSuite.cpp
@@ -20,7 +20,7 @@ namespace {
 
         void acceptProposal() {};
 
-        [[nodiscard]] double computeLogAcceptanceProbability() const {
+        [[nodiscard]] double calculateLogAcceptanceProbability() const {
             return std::log(1 - stepSize);
         };
 
@@ -68,33 +68,24 @@ BOOST_AUTO_TEST_SUITE(AcceptanceRateTuner)
         double targetAcceptanceRate = 0.825;
         double lowerLimitStepSize = 1e-2;
         double upperLimitStepSize = 1;
-        double smoothingLength = 1;
-        size_t iterationsToTestStepSize = 100;
-        size_t maxPosteriorUpdates = 20;
-        size_t maxPureSamplingRounds = 1;
-        size_t iterationsForConvergence = 5;
-
-        hops::AcceptanceRateTuner::param_type parameters{
-                        targetAcceptanceRate,
-                        iterationsToTestStepSize,
-                        maxPosteriorUpdates,
-                        maxPureSamplingRounds,
-                        iterationsForConvergence,
-                        200,
-                        lowerLimitStepSize,
-                        upperLimitStepSize,
-                        smoothingLength,
-                        42,
-                        false
-                };
+        size_t iterationsToTestStepSize = 50;
+        size_t maxIterations = 100;
 
         std::vector<std::shared_ptr<hops::MarkovChain>> mcs{markovChain};
         std::vector<hops::RandomNumberGenerator> generators{generator};
         bool isTuned = hops::AcceptanceRateTuner::tune(
                 mcs,
                 generators,
-                parameters
-                );
+                {
+                        targetAcceptanceRate,
+                        iterationsToTestStepSize,
+                        maxIterations,
+                        200,
+                        lowerLimitStepSize,
+                        upperLimitStepSize,
+                        42,
+                        "test_output"
+                });
 
 
         markovChain->draw(generator, 5000);
@@ -102,7 +93,7 @@ BOOST_AUTO_TEST_SUITE(AcceptanceRateTuner)
         std::cout<< actualAcceptanceRate << std::endl;
         BOOST_CHECK(isTuned);
         BOOST_CHECK_LE(markovChain->getNumberOfStepsTaken(),
-                       maxPosteriorUpdates *
+                       maxIterations *
                        iterationsToTestStepSize + 5000);
         double upperLimitAcceptanceRate = 0.85;
         double lowerLimitAcceptanceRate = 0.75;
