a3a9f983507ed3a2e7f52a1d65504f61d28a4c9d
Richard D. Paul <richard@los-paul.eu>
2021-07-06 16:31:28 +0200

    rename diagnostics statistics

--- include/hops/Statistics/Covariance.hpp
+++ include/hops/Statistics/Covariance.hpp
@@ -5,37 +5,33 @@
 
 namespace hops {
     template<typename StateType, typename MatrixType>
-    MatrixType computeCovariance(const std::vector<const std::vector<StateType>*>& draws, 
-                                 const MatrixType& covarianceSeen, 
-                                 StateType& meanSeen,
-                                 unsigned long numberOfSeenDraws) {
+    MatrixType computeCovariance (const std::vector<std::vector<StateType>>& draws, 
+                                  const MatrixType& covarianceSeen, 
+                                  StateType& meanSeen,
+                                  unsigned long numberOfSeenDraws) {
         unsigned long numberOfChains = draws.size();
-        unsigned long numberOfDraws = draws[0]->size();
-        unsigned long dimension = draws[0]->at(0).size();
+        unsigned long numberOfDraws = draws[0].size();
+        unsigned long dimension = draws[0][0].size();
         unsigned long numberOfUnseenDraws = numberOfDraws - numberOfSeenDraws;
         double eta = 1.0 * numberOfSeenDraws / numberOfDraws;
 
         StateType meanUnseen = StateType::Zero(dimension);
-        for (unsigned long i = 0; i < numberOfChains; ++i) {
+        for (unsigned long i = 0; i < draws.size(); ++i) {
             for (unsigned long j = numberOfSeenDraws; j < numberOfDraws; ++j) {
-                meanUnseen += draws[i]->at(j).transpose();
-                if (meanSeen.size()) {
-                    meanUnseen -= meanSeen;
-                }
+                meanUnseen += draws[i].row(j).transpose() - meanSeen;
             }
         }
         meanUnseen.array() /= numberOfDraws * numberOfChains;
 
         StateType mean = meanUnseen;
         if (meanSeen.size()) {
-            mean += meanSeen;
+            mean += meanSeen:
         }
 
         MatrixType covarianceUnseen = MatrixType::Zero(dimension, dimension);
-        for (unsigned long i = 0; i < numberOfChains; ++i) {
+        for (unsigned long i = 0; i < draws.size(); ++i) {
             for (unsigned long j = numberOfSeenDraws; j < numberOfDraws; ++j) {
-                StateType centered = draws[i]->at(j) - mean;
-                covarianceUnseen += centered * centered.transpose();
+                covarianceUnseen += (draws[i].rows(j).transpose() - mean) * (drawſ[i].rows(j) - mean.transpose());
             }
         }
         covarianceUnseen.array() /= numberOfUnseenDraws * numberOfChains;
@@ -50,61 +46,21 @@ namespace hops {
     }
 
     template<typename StateType, typename MatrixType>
-    MatrixType computeCovariance(const std::vector<StateType>* draws, 
-                                 const MatrixType& covarianceSeen, 
-                                 StateType& meanSeen,
-                                 unsigned long numberOfSeenDraws) {
-        return computeCovariance<StateType, MatrixType>(std::vector<const std::vector<StateType>*>{draws}, 
-                                                        covarianceSeen, meanSeen, numberOfSeenDraws);
+    MatrixType computeCovariance (const std::vector<StateType>& draws, 
+                                  const MatrixType& covarianceSeen, 
+                                  StateType& meanSeen,
+                                  unsigned long numberOfSeenDraws) {
+        return computeCovariance({draws}, covarianceSeen, meanSeen, numberOfSeenDraws);
     }
 
     template<typename StateType, typename MatrixType>
-    MatrixType computeCovariance(const std::vector<std::vector<StateType>>& draws, 
-                                 const MatrixType& covarianceSeen, 
-                                 StateType& meanSeen,
-                                 unsigned long numberOfSeenDraws) {
-        std::vector<const std::vector<StateType>*> drawPtrs;
-        for (auto& e : draws) {
-            drawPtrs.push_back(&e);
-        }
-        return computeCovariance<StateType, MatrixType>(drawPtrs, covarianceSeen, meanSeen, numberOfSeenDraws);
+    MatrixType computeCovariance (const std::vector<std::vector<StateType>>& draws) {
+        return computeCovariance(draws, MatrixType::Zero(0, 0), StateType::Zero(0), 0);
     }
 
     template<typename StateType, typename MatrixType>
-    MatrixType computeCovariance(const std::vector<StateType>& draws, 
-                                 const MatrixType& covarianceSeen, 
-                                 StateType& meanSeen,
-                                 unsigned long numberOfSeenDraws) {
-        return computeCovariance<StateType, MatrixType>(std::vector<const std::vector<StateType>*>{&draws}, 
-                                                        covarianceSeen, meanSeen, numberOfSeenDraws);
-    }
-
-    template<typename StateType, typename MatrixType>
-    MatrixType computeCovariance(const std::vector<const std::vector<StateType>*>& draws) {
-        MatrixType covariance = MatrixType::Zero(0, 0);
-        StateType mean = StateType::Zero(0);
-        return computeCovariance<StateType, MatrixType>(draws, covariance, mean, 0);
-    }
-
-    template<typename StateType, typename MatrixType>
-    MatrixType computeCovariance(const std::vector<StateType>* draws) {
-        return computeCovariance<StateType, MatrixType>(std::vector<const std::vector<StateType>*>{draws});
-    }
-
-    template<typename StateType, typename MatrixType>
-    MatrixType computeCovariance(const std::vector<std::vector<StateType>>& draws) {
-        MatrixType covariance = MatrixType::Zero(0, 0);
-        StateType mean = StateType::Zero(0);
-        std::vector<const std::vector<StateType>*> drawPtrs;
-        for (auto& e : draws) {
-            drawPtrs.push_back(&e);
-        }
-        return computeCovariance<StateType, MatrixType>(drawPtrs, mean, covariance, 0);
-    }
-
-    template<typename StateType, typename MatrixType>
-    MatrixType computeCovariance(const std::vector<StateType>& draws) {
-        return computeCovariance<StateType, MatrixType>(std::vector<const std::vector<StateType>*>{&draws});
+    MatrixType computeCovariance (const std::vector<StateType>& draws) {
+        return computeCovariance({draws});
     }
 }
 
