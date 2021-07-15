e4f0475151958b41cc36150e7447a70eae626616
Richard D. Paul <richard@los-paul.eu>
2021-07-15 12:23:33 +0200

    handle constant chain case correctly when computing esjd

--- include/hops/Statistics/IsConstantChain.hpp
+++ include/hops/Statistics/IsConstantChain.hpp
@@ -22,8 +22,75 @@ namespace hops {
     }
 
     template<typename StateType>
+    bool isConstantChain (const std::vector<const std::vector<StateType>*>& chains, long dimension) {
+        using Scalar = typename StateType::Scalar;
+
+        long d = dimension;
+        unsigned long numberOfChains = chains.size();
+        unsigned long numberOfDraws = chains[0]->size();
+       
+        std::vector<Scalar> initDraw(numberOfChains, 0);
+
+        bool chainsAllConst = true;
+        for (unsigned long m = 0; m < numberOfChains; ++m) {
+            initDraw[m] = (*chains[m])[0](d);
+
+            if (!std::isfinite(initDraw[m])) {
+                return std::numeric_limits<double>::quiet_NaN();
+            }
+
+            // check if all draws are (almost) equal to the first value.
+            // if one is different, break.
+            bool drawsAllConst = true;
+            for (unsigned long n = 1; n < numberOfDraws; ++n) {
+                if (!std::isfinite((*chains[m])[n](d))) {
+                    return true;
+                }
+
+                if (!almost_equal(initDraw[m], (*chains[m])[n](d))) {
+                    drawsAllConst = false;
+                    break;
+                }
+            }
+
+            // record if any of the chains has draws which are not all const
+            if (!drawsAllConst) {
+                chainsAllConst = false;
+                break;
+            }
+        }
+
+        if (chainsAllConst) {
+            bool chainsAllSameConst = true;
+            // If all chains are constant then return NaN
+            // if they all equal the same constant value
+            for (unsigned long m = 1; m < numberOfChains; ++m) {
+                if (!almost_equal(initDraw[0], initDraw[m])) {
+                    chainsAllSameConst = false;
+                    break;
+                }
+            }
+
+            if (chainsAllSameConst) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    template<typename StateType>
+    bool isConstantChain (const std::vector<std::vector<StateType>>& chains, long dimension) {
+        std::vector<const std::vector<StateType>*> chainsPtrArray;
+        for (auto& chain : chains) {
+            chainsPtrArray.push_back(&chain);
+        }
+        return isConstantChain(chainsPtrArray, dimension);
+    }
+
+    template<typename StateType>
     bool isConstantChain (const std::vector<const std::vector<StateType>*>& chains) {
-        for (size_t dimension; dimension < (*chains[0])[0].size(); ++dimension) {
+        for (long dimension = 0; dimension < (*chains[0])[0].size(); ++dimension) {
             if (!isConstantChain(chains, dimension)) {
                 return false;
             }
@@ -39,73 +106,6 @@ namespace hops {
             chainsPtrArray.push_back(&chain);
         }
         return isConstantChain(chainsPtrArray);
-    }
-
-    template<typename StateType>
-    bool isConstantChain (const std::vector<const std::vector<StateType>*>& chains, unsigned long dimension) {
-        using Scalar = typename StateType::Scalar;
-
-        unsigned long d = dimension;
-        unsigned long numChains = chains.size();
-        unsigned long numDraws = chains[0]->size();
-       
-        std::vector<Scalar> initDraw(numChains, 0);
-
-	bool chainsAllConst = true;
-	for (unsigned long m = 0; m < numChains; ++m) {
-	    initDraw[m] = (*chains[m])[0](d);
-
-            if (!std::isfinite(initDraw[m])) {
-                return std::numeric_limits<double>::quiet_NaN();
-            }
-
-            // check if all draws are (almost) equal to the first value.
-            // if one is different, break.
-            bool drawsAllConst = true;
-            for (unsigned long n = 1; n < numDraws; ++n) {
-		if (!std::isfinite((*chains[m])[n](d))) {
-                    return true;
-		}
-
-                if (!almost_equal(initDraw[m], (*chains[m])[n](d))) {
-                    drawsAllConst = false;
-                    break;
-                }
-            }
-
-            // record if any of the chains has draws which are not all const
-	    if (!drawsAllConst) {
-		chainsAllConst = false;
-                break;
-	    }
-	}
-
-	if (chainsAllConst) {
-            bool chainsAllSameConst = true;
-	    // If all chains are constant then return NaN
-	    // if they all equal the same constant value
-            for (unsigned long m = 1; m < numChains; ++m) {
-                if (!almost_equal(initDraw[0], initDraw[m])) {
-                    chainsAllSameConst = false;
-                    break;
-                }
-            }
-
-	    if (chainsAllSameConst) {
-                return true;
-	    }
-	}
-
-	return false;
-    }
-
-    template<typename StateType>
-    bool isConstantChain (const std::vector<std::vector<StateType>>& chains, unsigned long dimension) {
-        std::vector<const std::vector<StateType>*> chainsPtrArray;
-        for (auto& chain : chains) {
-            chainsPtrArray.push_back(&chain);
-        }
-        return isConstantChain(chainsPtrArray, dimension);
     }
 }
 
