65395f5e87f4207ae48f72ec2b5e729e46807ba7
Jadebeck, Johann Fredrik <j.jadebeck@fz-juelich.de>
2021-08-24 14:52:44 +0200

    add simplex benchmarker

--- include/hops/MarkovChain/MarkovChainType.hpp
+++ include/hops/MarkovChain/MarkovChainType.hpp
@@ -11,6 +11,46 @@ namespace hops {
         Gaussian,
         HitAndRun,
     };
+
+
+    std::string MarkovChainTypeToFullString(MarkovChainType markovChainType) {
+        switch(markovChainType) {
+            case MarkovChainType::AdaptiveMetropolis:
+                return "Adaptive Metropolis";
+            case MarkovChainType::BallWalk:
+                return "Ball Walk";
+            case MarkovChainType::CoordinateHitAndRun:
+                return "Coordinate Hit-and-Run";
+            case MarkovChainType::CSmMALA:
+                return "Constrained Simplified manifold Metropolis Adjusted Langevin Algorithm";
+            case MarkovChainType::DikinWalk:
+                return "Dikin Walk";
+            case MarkovChainType::Gaussian:
+                return "Gaussian Random Walk";
+            case MarkovChainType::HitAndRun:
+                return "Hit-and-Run";
+        }
+    }
+
+    std::string MarkovChainTypeToShortcutString(MarkovChainType markovChainType) {
+        switch(markovChainType) {
+            case MarkovChainType::AdaptiveMetropolis:
+                return "AM";
+            case MarkovChainType::BallWalk:
+                return "BW";
+            case MarkovChainType::CoordinateHitAndRun:
+                return "CHR";
+            case MarkovChainType::CSmMALA:
+                return "CSmMALA";
+            case MarkovChainType::DikinWalk:
+                return "DW";
+            case MarkovChainType::Gaussian:
+                return "G";
+            case MarkovChainType::HitAndRun:
+                return "HR";
+        }
+
+    }
 }
 
 #endif //HOPS_MARKOVCHAINTYPE_HPP
