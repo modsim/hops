a13f552c640d3fe25c037b4402e5a3d47199f5a8
Jadebeck, Johann Fredrik <j.jadebeck@fz-juelich.de>
2021-11-19 01:33:07 +0100

    finished gamma

--- src/hops/MarkovChain/MarkovChainAdapter.hpp
+++ src/hops/MarkovChain/MarkovChainAdapter.hpp
@@ -12,131 +12,130 @@
 #include "IsSetFisherWeightAvailable.hpp"
 #include "hops/MarkovChain/Proposal/IsSetStepSizeAvailable.hpp"
 
+namespace hops {
+    template<typename MarkovChainImpl>
+    class MarkovChainAdapter : public MarkovChain, public MarkovChainImpl {
+    public:
+        explicit MarkovChainAdapter(const MarkovChainImpl &markovChainImpl) : MarkovChainImpl(markovChainImpl) {}
 
-//namespace hops {
-//    template<typename MarkovChainImpl>
-//    class MarkovChainAdapter : public MarkovChain, public MarkovChainImpl {
-//    public:
-//        explicit MarkovChainAdapter(const MarkovChainImpl &markovChainImpl) : MarkovChainImpl(markovChainImpl) {}
-//
-//        void draw(RandomNumberGenerator &randomNumberGenerator, long numberOfSamples) override {
-//            assert(numberOfSamples > 0);
-//            for (long i = 0; i < numberOfSamples; ++i) {
-//                MarkovChainImpl::draw(randomNumberGenerator);
-//                MarkovChainImpl::storeRecord();
-//            }
-//        }
-//
-//        void draw(RandomNumberGenerator &randomNumberGenerator, long numberOfSamples, long thinning) override {
-//            assert(numberOfSamples > 0);
-//            assert(thinning > 0);
-//            for (long i = 0; i < numberOfSamples; ++i) {
-//                for (long j = 0; j < thinning; ++j) {
-//                    MarkovChainImpl::draw(randomNumberGenerator);
-//                }
-//                MarkovChainImpl::storeRecord();
-//            }
-//        }
-//
-//        void writeHistory(FileWriter *const fileWriter) override {
-//            MarkovChainImpl::writeRecordsToFile(fileWriter);
-//        }
-//
-//        void installDataObject(ChainData &chainData) override {
-//            MarkovChainImpl::installDataObject(chainData);
-//        }
-//
-//        const std::vector<Eigen::VectorXd> &getStateRecords() override {
-//            return MarkovChainImpl::getStateRecords();
-//        }
-//
-//        void reserveStateRecords(long numberOfSamples) override {
-//            return MarkovChainImpl::reserveStateRecords(numberOfSamples);
-//        }
-//
-//        void clearHistory() override {
-//            MarkovChainImpl::clearRecords();
-//        }
-//
-//        std::string getName() override {
-//            return MarkovChainImpl::getName();
-//        }
-//
-//        double getAcceptanceRate() override {
-//            return MarkovChainImpl::getAcceptanceRate();
-//        }
-//
-//        void setAttribute(MarkovChainAttribute markovChainAttribute, double value) override {
-//            switch (markovChainAttribute) {
-//                case MarkovChainAttribute::FISHER_WEIGHT: {
-//                    if constexpr(IsSetFisherWeightAvailable<MarkovChainImpl>::value) {
-//                        MarkovChainImpl::setFisherWeight(value);
-//                        break;
-//                    }
-//                    throw std::runtime_error("FISHER_WEIGHT attribute does not exist.");
-//                }
-//                case MarkovChainAttribute::STEP_SIZE: {
-//                    if constexpr(IsSetStepSizeAvailable<MarkovChainImpl>::value) {
-//                        MarkovChainImpl::setStepSize(value);
-//                        break;
-//                    }
-//                    throw std::runtime_error("STEP_SIZE attribute does not exist.");
-//                }
-//                case MarkovChainAttribute::PARALLEL_TEMPERING_COLDNESS: {
-//                    if constexpr(IsSetColdnessAvailable<MarkovChainImpl>::value) {
-//                        MarkovChainImpl::setColdness(value);
-//                        break;
-//                    }
-//                    throw std::runtime_error("PARALLEL_TEMPERING_COLDNESS attribute does not exist.");
-//
-//                }
-//                case MarkovChainAttribute::PARALLEL_TEMPERING_EXCHANGE_PROBABILITY: {
-//                    if constexpr(IsSetExchangeAttemptProbabilityAvailable<MarkovChainImpl>::value) {
-//                        MarkovChainImpl::setExchangeAttemptProbability(value);
-//                        break;
-//                    }
-//                    throw std::runtime_error("PARALLEL_TEMPERING_EXCHANGE_PROBABILITY attribute does not exist.");
-//                }
-//                default: {
-//                    throw std::runtime_error("Attribute does not exist.");
-//                }
-//            }
-//        }
-//
-//        void setState(Eigen::Matrix<double, -1, 1, 0, -1, 1> state) override {
-//            MarkovChainImpl::setState(state);
-//        }
-//
-//        double getAttribute(MarkovChainAttribute markovChainAttribute) override {
-//            switch (markovChainAttribute) {
-//                case MarkovChainAttribute::PARALLEL_TEMPERING_COLDNESS: {
-//                    if constexpr(IsGetColdnessAvailable<MarkovChainImpl>::value) {
-//                        return MarkovChainImpl::getColdness();
-//                    }
-//                    throw std::runtime_error("PARALLEL_TEMPERING_COLDNESS attribute does not exist.");
-//
-//                }
-//                case MarkovChainAttribute::PARALLEL_TEMPERING_EXCHANGE_PROBABILITY: {
-//                    if constexpr(IsGetExchangeAttemptProbabilityAvailable<MarkovChainImpl>::value) {
-//                        return MarkovChainImpl::getExchangeAttemptProbability();
-//                    }
-//                    throw std::runtime_error("PARALLEL_TEMPERING_EXCHANGE_PROBABILITY attribute does not exist.");
-//                }
-//                case MarkovChainAttribute::PREVIOUS_STEP_ACCEPTANCE_PROBABILITY: {
-//                   return MarkovChainImpl::computeLogAcceptanceProbability();
-//                }
-//                case MarkovChainAttribute::STEP_SIZE: {
-//                    if constexpr(IsGetStepSizeAvailable<MarkovChainImpl>::value) {
-//                        return MarkovChainImpl::getStepSize();
-//                    }
-//                    throw std::runtime_error("STEP_SIZE attribute does not exist.");
-//                }
-//                default: {
-//                    throw std::runtime_error("Attribute does not exist.");
-//                }
-//            }
-//        }
-//    };
-//}
+        void draw(RandomNumberGenerator &randomNumberGenerator, long numberOfSamples) override {
+            assert(numberOfSamples > 0);
+            for (long i = 0; i < numberOfSamples; ++i) {
+                MarkovChainImpl::draw(randomNumberGenerator);
+                MarkovChainImpl::storeRecord();
+            }
+        }
+
+        void draw(RandomNumberGenerator &randomNumberGenerator, long numberOfSamples, long thinning) override {
+            assert(numberOfSamples > 0);
+            assert(thinning > 0);
+            for (long i = 0; i < numberOfSamples; ++i) {
+                for (long j = 0; j < thinning; ++j) {
+                    MarkovChainImpl::draw(randomNumberGenerator);
+                }
+                MarkovChainImpl::storeRecord();
+            }
+        }
+
+        void writeHistory(FileWriter *const fileWriter) override {
+            MarkovChainImpl::writeRecordsToFile(fileWriter);
+        }
+
+        void installDataObject(ChainData &chainData) override {
+            MarkovChainImpl::installDataObject(chainData);
+        }
+
+        const std::vector<Eigen::VectorXd> &getStateRecords() override {
+            return MarkovChainImpl::getStateRecords();
+        }
+
+        void reserveStateRecords(long numberOfSamples) override {
+            return MarkovChainImpl::reserveStateRecords(numberOfSamples);
+        }
+
+        void clearHistory() override {
+            MarkovChainImpl::clearRecords();
+        }
+
+        std::string getName() override {
+            return MarkovChainImpl::getName();
+        }
+
+        double getAcceptanceRate() override {
+            return MarkovChainImpl::getAcceptanceRate();
+        }
+
+        void setAttribute(MarkovChainAttribute markovChainAttribute, double value) override {
+            switch (markovChainAttribute) {
+                case MarkovChainAttribute::FISHER_WEIGHT: {
+                    if constexpr(IsSetFisherWeightAvailable<MarkovChainImpl>::value) {
+                        MarkovChainImpl::setFisherWeight(value);
+                        break;
+                    }
+                    throw std::runtime_error("FISHER_WEIGHT attribute does not exist.");
+                }
+                case MarkovChainAttribute::STEP_SIZE: {
+                    if constexpr(IsSetStepSizeAvailable<MarkovChainImpl>::value) {
+                        MarkovChainImpl::setStepSize(value);
+                        break;
+                    }
+                    throw std::runtime_error("STEP_SIZE attribute does not exist.");
+                }
+                case MarkovChainAttribute::PARALLEL_TEMPERING_COLDNESS: {
+                    if constexpr(IsSetColdnessAvailable<MarkovChainImpl>::value) {
+                        MarkovChainImpl::setColdness(value);
+                        break;
+                    }
+                    throw std::runtime_error("PARALLEL_TEMPERING_COLDNESS attribute does not exist.");
+
+                }
+                case MarkovChainAttribute::PARALLEL_TEMPERING_EXCHANGE_PROBABILITY: {
+                    if constexpr(IsSetExchangeAttemptProbabilityAvailable<MarkovChainImpl>::value) {
+                        MarkovChainImpl::setExchangeAttemptProbability(value);
+                        break;
+                    }
+                    throw std::runtime_error("PARALLEL_TEMPERING_EXCHANGE_PROBABILITY attribute does not exist.");
+                }
+                default: {
+                    throw std::runtime_error("Attribute does not exist.");
+                }
+            }
+        }
+
+        void setState(Eigen::Matrix<double, -1, 1, 0, -1, 1> state) override {
+            MarkovChainImpl::setState(state);
+        }
+
+        double getAttribute(MarkovChainAttribute markovChainAttribute) override {
+            switch (markovChainAttribute) {
+                case MarkovChainAttribute::PARALLEL_TEMPERING_COLDNESS: {
+                    if constexpr(IsGetColdnessAvailable<MarkovChainImpl>::value) {
+                        return MarkovChainImpl::getColdness();
+                    }
+                    throw std::runtime_error("PARALLEL_TEMPERING_COLDNESS attribute does not exist.");
+
+                }
+                case MarkovChainAttribute::PARALLEL_TEMPERING_EXCHANGE_PROBABILITY: {
+                    if constexpr(IsGetExchangeAttemptProbabilityAvailable<MarkovChainImpl>::value) {
+                        return MarkovChainImpl::getExchangeAttemptProbability();
+                    }
+                    throw std::runtime_error("PARALLEL_TEMPERING_EXCHANGE_PROBABILITY attribute does not exist.");
+                }
+                case MarkovChainAttribute::PREVIOUS_STEP_ACCEPTANCE_PROBABILITY: {
+                   return MarkovChainImpl::computeLogAcceptanceProbability();
+                }
+                case MarkovChainAttribute::STEP_SIZE: {
+                    if constexpr(IsGetStepSizeAvailable<MarkovChainImpl>::value) {
+                        return MarkovChainImpl::getStepSize();
+                    }
+                    throw std::runtime_error("STEP_SIZE attribute does not exist.");
+                }
+                default: {
+                    throw std::runtime_error("Attribute does not exist.");
+                }
+            }
+        }
+    };
+}
 
 #endif //HOPS_MARKOVCHAINADAPTER_HPP
--- tests/MarkovChain/ReversibleJump/ReversibleJumpProposalTestSuite.cpp
+++ tests/MarkovChain/ReversibleJump/ReversibleJumpProposalTestSuite.cpp
@@ -2,18 +2,265 @@
 #define BOOST_TEST_DYN_LINK
 
 #include <boost/test/included/unit_test.hpp>
+#include <cmath>
+#include <Eigen/Core>
+#include <utility>
+#include <vector>
+
+
 #include <hops/MarkovChain/ReversibleJump/ReversibleJumpProposal.hpp>
-#include <hops/Model/GammaModels.hpp>
 #include <hops/MarkovChain/MarkovChainAdapter.hpp>
 #include <hops/MarkovChain/Recorder/StateRecorder.hpp>
 #include <hops/MarkovChain/Proposal/HitAndRunProposal.hpp>
+#include <hops/Utility/MatrixType.hpp>
+#include <hops/Utility/VectorType.hpp>
+
+namespace {
+    double gammaProbabilityDensityFunction(double x, double location, double scale, double shape) {
+        if (scale <= 0 || shape <= 0) {
+            throw std::runtime_error("scale and shape parameters have to be larger than 0.");
+        }
+        if (x - location < 0) {
+            return 0;
+        }
+        return (std::pow(x - location, shape - 1) * std::exp(-(x - location) / scale)) /
+               (std::tgamma(shape) * std::pow(scale, shape));
+    }
+
+    class FullGammaModel {
+    public:
+        using FloatType = double;
+        using MatrixType = Eigen::MatrixXd;
+        using VectorType = Eigen::VectorXd;
+
+        explicit FullGammaModel(std::vector<FloatType> measurements) : measurements(std::move(measurements)) {
+            A = MatrixType(6, 3);
+            A << 1, 0, 0,
+                    -1, 0, 0,
+                    0, 1, 0,
+                    0, -1, 0,
+                    0, 0, 1,
+                    0, 0, -1;
+
+            b = VectorType(6);
+            b << 0.9, 0., 10, -0.1, 10, -0.1;
+        }
+
+        [[nodiscard]] FloatType computeNegativeLogLikelihood(const VectorType &parameters) const {
+            FloatType neglike = 0;
+            for (const auto &measurement : this->measurements) {
+                neglike -= std::log(
+                        gammaProbabilityDensityFunction(measurement,
+                                                        parameters(0),
+                                                        parameters(1),
+                                                        parameters(2)));
+
+            }
+            return neglike;
+        }
+
+        [[nodiscard]] const std::vector<std::string> &getParameterNames() const {
+            return parameterNames;
+        }
+
+        VectorType getB() const {
+            return b;
+        }
+
+        MatrixType getA() const {
+            return A;
+        }
+
+        [[nodiscard]] const std::string &getModelName() const {
+            return modelName;
+        }
+
+    private:
+        std::vector<std::string> parameterNames = {"location", "scale", "shape"};
+
+        VectorType b;
+        MatrixType A;
+        std::vector<FloatType> measurements;
+        std::string modelName = "FullGammaModel";
+    };
+
+
+    class GammaModel1 {
+    public:
+        using FloatType = double;
+        using MatrixType = Eigen::MatrixXd;
+        using VectorType = Eigen::VectorXd;
+
+        explicit GammaModel1(std::vector<FloatType> measurements) : measurements(std::move(measurements)) {
+            A = MatrixType(4, 2);
+            A << 1, 0,
+                    -1, 0,
+                    0, 1,
+                    0, -1;
+
+            b = VectorType(4);
+            b << 0.9, 0., 10, -0.1;
+        }
+
+        FloatType computeNegativeLogLikelihood(const VectorType &parameters) const {
+            FloatType neglike = 0;
+            for (const auto &measurement : this->measurements) {
+                neglike -= std::log(
+                        gammaProbabilityDensityFunction(measurement,
+                                                        parameters(0),
+                                                        scale,
+                                                        parameters(1)));
+
+            }
+            return neglike;
+        }
+
+        [[nodiscard]] const std::vector<std::string> &getParameterNames() const {
+            return parameterNames;
+        }
+
+        VectorType getB() const {
+            return b;
+        }
+
+        MatrixType getA() const {
+            return A;
+        }
+
+        [[nodiscard]] const std::string &getModelName() const {
+            return modelName;
+        }
+
+    private:
+        std::vector<std::string> parameterNames = {"location", "shape"};
+
+        VectorType b;
+        MatrixType A;
+        std::vector<FloatType> measurements;
+        constexpr static const double scale = 1;
+        std::string modelName = "GammaModel1";
+    };
+
+    class GammaModel2 {
+    public:
+        using FloatType = double;
+        using MatrixType = Eigen::MatrixXd;
+        using VectorType = Eigen::VectorXd;
+
+        explicit GammaModel2(std::vector<FloatType> measurements) : measurements(std::move(measurements)) {
+            A = MatrixType(4, 2);
+            A << 1, 0,
+                    -1, 0,
+                    0, 1,
+                    0, -1;
+
+            b = VectorType(4);
+            b << 10, -0.1, 10, -0.1;
+        }
+
+        [[nodiscard]] FloatType computeNegativeLogLikelihood(const VectorType &parameters) const {
+            FloatType neglike = 0;
+            for (const auto &measurement : this->measurements) {
+                neglike -= std::log(
+                        gammaProbabilityDensityFunction(measurement,
+                                                        location,
+                                                        parameters(0),
+                                                        parameters(1)));
+
+            }
+            return neglike;
+        }
+
+        [[nodiscard]] const std::vector<std::string> &getParameterNames() const {
+            return parameterNames;
+        }
+
+        [[nodiscard]] VectorType getB() const {
+            return b;
+        }
+
+        [[nodiscard]] MatrixType getA() const {
+            return A;
+        }
+
+        [[nodiscard]] const std::string &getModelName() const {
+            return modelName;
+        }
+
+    private:
+        std::vector<std::string> parameterNames = {"scale", "shape"};
+
+        VectorType b;
+        MatrixType A;
+        std::vector<FloatType> measurements;
+
+        constexpr static double location = 0;
+        std::string modelName = "GammaModel2";
+    };
+
+
+    class MinimalGammaModel {
+    public:
+        using FloatType = double;
+        using MatrixType = Eigen::MatrixXd;
+        using VectorType = Eigen::VectorXd;
+
+        explicit MinimalGammaModel(std::vector<FloatType> measurements) : measurements(std::move(measurements)) {
+            A = MatrixType(2, 1);
+            A << 1, -1;
+            b = VectorType(2);
+            b << 10, -0.1;
+        }
+
+        [[nodiscard]] FloatType computeNegativeLogLikelihood(const VectorType &parameters) const {
+            FloatType neglike = 0;
+            for (const auto &measurement : this->measurements) {
+                neglike -= std::log(
+                        gammaProbabilityDensityFunction(measurement,
+                                                        location,
+                                                        scale,
+                                                        parameters(0)));
+
+            }
+            return neglike;
+        }
+
+        [[nodiscard]] const std::vector<std::string> &getParameterNames() const {
+            return parameterNames;
+        }
+
+        [[nodiscard]] VectorType getB() const {
+            return b;
+        }
+
+        [[nodiscard]] MatrixType getA() const {
+            return A;
+        }
+
+        [[nodiscard]] const std::string &getModelName() const {
+            return modelName;
+        }
+
+    private:
+        std::vector<std::string> parameterNames = {"shape"};
+
+        VectorType b;
+        MatrixType A;
+        std::vector<FloatType> measurements;
+
+        constexpr static double location = 0;
+        constexpr static double scale = 1;
+        std::string modelName = "MinimalGammaModel";
+    };
+}
 
 BOOST_AUTO_TEST_SUITE(ReversibleJumpProposal)
 
+// TODO fix to work with updated api
 //    BOOST_AUTO_TEST_CASE(GammaModel) {
 //        // The gamma model is a simple test case where the RJMCMC algorithm jumps a nested model structure of gamma functions.
 //        double measurement = 1;
-//        hops::FullGammaModel model({measurement});
+//        FullGammaModel model({measurement});
 //        auto A = model.getA();
 //        auto b = model.getB();
 //
@@ -74,21 +321,19 @@ BOOST_AUTO_TEST_SUITE(ReversibleJumpProposal)
 //
 //        // Actual model probabilities integrated with scipy and rounded:
 //        std::vector<int> expectedModelProbabilityPercentages{
-//            18, // 0.18335007
-//            34, // 0.34859468
-//            16, // 0.15325739
-//            31, // 0.31479786
+//                18, // 0.18335007
+//                34, // 0.34859468
+//                16, // 0.15325739
+//                31, // 0.31479786
 //        };
 //
 //        BOOST_ASSERT(actualModelProbabilities.size() == expectedModelProbabilityPercentages.size());
-//        for(long i=0; i<actualModelProbabilities.size(); ++i) {
+//        for (long i = 0; i < actualModelProbabilities.size(); ++i) {
 //            BOOST_CHECK_EQUAL(
 //                    actualModelProbabilities[i],
 //                    expectedModelProbabilityPercentages[i]
 //            );
 //        }
-//
-//        // TODO add checks the parameter inferences are correct
 //    }
 
 BOOST_AUTO_TEST_SUITE_END()
