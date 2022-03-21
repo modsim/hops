1d77576be3710973773baa3a41fa49100383f4b6
Jadebeck, Johann Fredrik <j.jadebeck@fz-juelich.de>
2022-03-21 15:37:38 +0100

    fix degenerate gaussian fisher information

--- tests/Model/DegenerateMultivariateGaussianTestSuite.cpp
+++ tests/Model/DegenerateMultivariateGaussianTestSuite.cpp
@@ -47,7 +47,7 @@ BOOST_AUTO_TEST_SUITE(DegenerateMultivariateGaussianModel)
             Eigen::VectorXd evaluationPoint2(3);
             evaluationPoint2 << evaluationPoint1(0) + 1e-5 * gradient.value()(0), evaluationPoint1(1) +
                                                                                   1e-5 * gradient.value()(1),
-                                                                                  evaluationPoint1(2);
+                    evaluationPoint1(2);
 
             // Tests if negative log likelihood decreases in gradient direction, thus checking correct sign of gradient.
             BOOST_CHECK_GT(model.computeNegativeLogLikelihood(evaluationPoint1),
@@ -58,6 +58,35 @@ BOOST_AUTO_TEST_SUITE(DegenerateMultivariateGaussianModel)
     }
 
     BOOST_AUTO_TEST_CASE(computeFisherInformation) {
+        Eigen::VectorXd mean(2);
+        mean << -.8, -.8;
+        Eigen::MatrixXd covariance(2, 2);
+        covariance << 8, 2, 2, 4;
+        Eigen::MatrixXd expectedExpectedFisherInformation(2, 2);
+        expectedExpectedFisherInformation << 1. / 7, -1. / 14, -1. / 14, 2. / 7;
+
+        hops::DegenerateGaussian gaussian(mean, covariance);
+
+        Eigen::VectorXd evaluationPoint1(2);
+        evaluationPoint1 << 0.8, 0.8;
+
+        auto actualExpectedFisherInformation1 = gaussian.computeExpectedFisherInformation(evaluationPoint1);
+        if (actualExpectedFisherInformation1) {
+            BOOST_CHECK(actualExpectedFisherInformation1.value().isApprox(expectedExpectedFisherInformation));
+        } else {
+            BOOST_FAIL("Fisher Info was not available.");
+        }
+
+        Eigen::VectorXd evaluationPoint2 = 5 * evaluationPoint1;
+        auto actualExpectedFisherInformation2 = gaussian.computeExpectedFisherInformation(evaluationPoint2);
+        if (actualExpectedFisherInformation2) {
+            BOOST_CHECK(actualExpectedFisherInformation2.value().isApprox(expectedExpectedFisherInformation));
+        } else {
+            BOOST_FAIL("Fisher Info was not available.");
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(computeFisherInformationWithInactiveIndices) {
         Eigen::VectorXd mean(3);
         mean << -.8, -.8, 123;
         Eigen::MatrixXd covariance(3, 3);
